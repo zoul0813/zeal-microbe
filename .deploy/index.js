cac89191c6cc4a378990c4f818b97bb017981c85
David Higgins <zoul0813@me.com>
2025-12-08 08:49:49 -0500

    update to build with ZDE, and to pull the romdisk from zshell

--- .deploy/index.js
+++ .deploy/index.js
@@ -1,115 +1,120 @@
 (() => {
-    // TODO: edit the user program here
-    const USER_PROGRAM = 'microbe.bin';
+  const ROMDISK_NAME = 'https://zoul0813.github.io/zeal-zshell/default.img'
+  const USER_PROGRAM = "microbe.bin";
 
-    console.log('Zeal Native Minimal Loading...');
+  console.log("Zeal Native Minimal Loading...");
 
-    const canvas = document.getElementById('canvas');
-    let moduleInstance = null;
-    function loadModule(userProgram) {
-        const defaultModule = {
-            arguments: ['-u', 'roms/user.bin'],
-            print: function(text) {
-                    console.log("Log: " + text);
-                },
-                printErr: function(text) {
-                    console.log("Error: " + text);
-            },
-            canvas: (function() {
-                return canvas;
-            })(),
-            onRuntimeInitialized: function() {
-                this.FS.writeFile('/roms/user.bin', userProgram);
-                canvas.setAttribute('tabindex', '0');
-                canvas.focus();
-            }
-        };
-        Module(defaultModule).then(mod => moduleInstance = mod);
-    }
+  const canvas = document.getElementById("canvas");
+  let moduleInstance = null;
+  function loadModule(romdiskImage, userProgram) {
+    const defaultModule = {
+      arguments: ["-r", "/default.img", "-u", "/user.bin"],
+      print: function (text) {
+        console.log("Log: " + text);
+      },
+      printErr: function (text) {
+        console.log("Error: " + text);
+      },
+      canvas: (function () {
+        return canvas;
+      })(),
+      onRuntimeInitialized: function () {
+        this.FS.writeFile("/default.img", romdiskImage);
+        this.FS.writeFile("/user.bin", userProgram);
+        canvas.setAttribute("tabindex", "0");
+        canvas.focus();
+      },
+    };
+    NativeModule(defaultModule).then((mod) => (moduleInstance = mod));
+  }
 
-    window.addEventListener('load', async () => {
-        await fetch(USER_PROGRAM).then((response) => {
-            if(!response.ok) throw new Error(`Failed to fetch ${USER_PROGRAM}`);
+  async function load() {
+    [romdiskImage, userProgram] = await Promise.all([
+        await fetch(ROMDISK_NAME).then((response) => {
+            if(!response.ok) throw new Error(`Failed to fetch ${ROMDISK_NAME}`);
             return response.arrayBuffer();
-        }).then((buffer) => {
-            console.log('buffer', buffer);
-            return new Uint8Array(buffer);
-        }).then(file => {
-            loadModule(file);
-        }).catch(err => console.error(err));
-    });
+        }).then((buffer) => new Uint8Array(buffer)),
+      await fetch(USER_PROGRAM)
+        .then((response) => {
+          if (!response.ok) throw new Error(`Failed to fetch ${USER_PROGRAM}`);
+          return response.arrayBuffer();
+        })
+        .then((buffer) => new Uint8Array(buffer))
+    ]).catch((err) => console.error(err));
+    loadModule(romdiskImage, userProgram);
+  }
 
-    function resumeAudioIfNeeded() {
-        if (Module.audioContext && Module.audioContext.state === 'suspended') {
-            Module.audioContext.resume().then(() => {
-                console.log("Audio context resumed");
-            });
-        }
+  window.addEventListener("load", load);
+
+  function resumeAudioIfNeeded() {
+    if (NativeModule.audioContext && NativeModule.audioContext.state === "suspended") {
+      NativeModule.audioContext.resume().then(() => {
+        console.log("Audio context resumed");
+      });
     }
+  }
 
+  const bStart = document.querySelector("#controls .buttons-start");
+  const bSelect = document.querySelector("#controls .buttons-select");
 
+  const bUp = document.querySelector("#controls .d-pad-up");
+  const bRight = document.querySelector("#controls .d-pad-right");
+  const bDown = document.querySelector("#controls .d-pad-down");
+  const bLeft = document.querySelector("#controls .d-pad-left");
 
-    const bStart = document.querySelector('#controls .buttons-start');
-    const bSelect = document.querySelector('#controls .buttons-select');
+  const bA = document.querySelector("#controls .buttons-a");
+  const bB = document.querySelector("#controls .buttons-b");
+  const bX = document.querySelector("#controls .buttons-x");
+  const bY = document.querySelector("#controls .buttons-y");
 
-    const bUp = document.querySelector('#controls .d-pad-up');
-    const bRight = document.querySelector('#controls .d-pad-right');
-    const bDown = document.querySelector('#controls .d-pad-down');
-    const bLeft = document.querySelector('#controls .d-pad-left');
+  const bL = document.querySelector("#controls .buttons-l");
+  const bR = document.querySelector("#controls .buttons-r");
 
-    const bA = document.querySelector('#controls .buttons-a');
-    const bB = document.querySelector('#controls .buttons-b');
-    const bX = document.querySelector('#controls .buttons-x');
-    const bY = document.querySelector('#controls .buttons-y');
+  function sendKey(type, key, code, keyCode) {
+    const e = {
+      key,
+      code,
+      keyCode,
+      which: keyCode,
+      bubbles: true,
+    };
+    console.log("key", type, key, code);
+    canvas.dispatchEvent(new KeyboardEvent(type, e));
+  }
 
-    const bL = document.querySelector('#controls .buttons-l');
-    const bR = document.querySelector('#controls .buttons-r');
+  function attachKeypressListener(el, key, code, keyCode) {
+    const onPress = (e) => {
+      e.preventDefault();
+      sendKey("keydown", key, code, keyCode);
+    };
+    const onRelease = (e) => {
+      e.preventDefault();
+      sendKey("keyup", key, code, keyCode);
+    };
 
-    function sendKey(type, key, code, keyCode) {
-        const e = {
-            key,
-            code,
-            keyCode,
-            which: keyCode,
-            bubbles: true,
-        };
-        console.log('key', type, key, code);
-        canvas.dispatchEvent(new KeyboardEvent(type, e));
-    }
+    el.addEventListener("mousedown", onPress);
+    el.addEventListener("mouseup", onRelease);
 
-    function attachKeypressListener(el, key, code, keyCode) {
-        const onPress = (e) => {
-            e.preventDefault();
-            sendKey('keydown', key, code, keyCode);
-        }
-        const onRelease = (e) => {
-            e.preventDefault();
-            sendKey('keyup', key, code, keyCode);
-        }
+    el.addEventListener("touchstart", onPress, { passive: false });
+    el.addEventListener("touchend", onRelease);
+    el.addEventListener("touchcancel", onRelease);
+  }
 
-        el.addEventListener('mousedown', onPress);
-        el.addEventListener('mouseup', onRelease);
+  attachKeypressListener(bStart, "Enter", "Enter", 13);
+  attachKeypressListener(bStart, "'", "Quote", 222);
 
-        el.addEventListener('touchstart', onPress, { passive: false });
-        el.addEventListener('touchend', onRelease);
-        el.addEventListener('touchcancel', onRelease);
-    }
+  attachKeypressListener(bUp, "ArrowUp", "ArrowUp", 38);
+  attachKeypressListener(bRight, "ArrowRight", "ArrowRight", 39);
+  attachKeypressListener(bDown, "ArrowDown", "ArrowDown", 40);
+  attachKeypressListener(bLeft, "ArrowLeft", "ArrowLeft", 37);
 
-    attachKeypressListener(bStart, "Enter", "Enter", 13);
-    attachKeypressListener(bStart, "'", "Quote", 222);
+  attachKeypressListener(bA, "x", "KeyX", 88); // X
+  attachKeypressListener(bB, "z", "KeyZ", 90); // Z
+  attachKeypressListener(bX, "s", "KeyS", 83); // S
+  attachKeypressListener(bY, "a", "KeyA", 65); // A
 
-    attachKeypressListener(bUp, "ArrowUp", "ArrowUp", 38);
-    attachKeypressListener(bRight, "ArrowRight", "ArrowRight", 39);
-    attachKeypressListener(bDown, "ArrowDown", "ArrowDown", 40);
-    attachKeypressListener(bLeft, "ArrowLeft", "ArrowLeft", 37);
+  attachKeypressListener(bL, "q", "KeyQ", 81); // Q
+  attachKeypressListener(bR, "w", "KeyW", 87); // W
 
-    attachKeypressListener(bA, "x", "KeyX", 88);   // X
-    attachKeypressListener(bB, "z", "KeyZ", 90);   // Z
-    attachKeypressListener(bX, "s", "KeyS", 83);   // S
-    attachKeypressListener(bY, "a", "KeyA", 65);   // A
-
-    attachKeypressListener(bL, "q", "KeyQ", 81);   // Q
-    attachKeypressListener(bR, "w", "KeyW", 87);   // W
-
-    console.log('Zeal Native Minimal Loaded!');
+  console.log("Zeal Native Minimal Loaded!");
 })();
